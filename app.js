0cf1ca3136ceaa59ed3bc8ce2e0c4b7627129e2c
Norman Sander <norman@nsander.de>
2016-03-06 20:13:21 +0100

    Delayed pull

--- app.js
+++ app.js
@@ -5,17 +5,21 @@ var express = require('express'),
 app.set('port', process.env.PORT || 8080);
 
 app.post('/deploy/', function (req, res) {
-     var spawn = require('child_process').spawn,
-        deploy = spawn('sh', [ './deploy.sh' ]);
+    var spawn = require('child_process').spawn;
+    var run_script = function(){
+        deploy = spawn('sh', [ './deploy.sh' ])
+        deploy.stdout.on('data', function (data) {
+            console.log(''+data);
+        });
 
-    deploy.stdout.on('data', function (data) {
-        console.log(''+data);
-    });
+        deploy.on('close', function (code) {
+            console.log('Child process exited with code ' + code);
+        });
+        res.json(200, {message: 'Github Hook received!'})
+    };
 
-    deploy.on('close', function (code) {
-        console.log('Child process exited with code ' + code);
-    });
-    res.json(200, {message: 'Github Hook received!'})
+    // Delayed call because new source is not available immmediately after webhook
+    setTimeout(run_script, 3000)
 });
 
 http.createServer(app).listen(app.get('port'), function(){
