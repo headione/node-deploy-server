82e3d1a2f82758124ab68c7a2e49847bca9f1645
Norman Sander <norman@nsander.de>
2018-01-07 15:04:16 +0100

    lint fixes

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
-var express = require('express'),
-    http = require('http'),
-    app = express();
+/* eslint-disable no-console */
+const express = require('express')
+const http = require('http')
+const app = express()
 
-app.set('port', process.env.PORT || 8080);
-
+app.set('port', process.env.PORT || 8080)
 app.post('/deploy/', function (req, res) {
-    var spawn = require('child_process').spawn,
-        deploy = spawn('sh', [ './deploy.sh' ])
+    const spawn = require('child_process').spawn
+    const deploy = spawn('sh', ['./deploy.sh'])
 
     deploy.stdout.on('data', function (data) {
-        console.log(''+data);
-    });
+        console.log(data)
+    })
 
     deploy.on('close', function (code) {
-        console.log('Child process exited with code ' + code);
-    });
+        console.log('Child process exited with code ' + code)
+    })
     res.json(200, {message: 'Github Hook received!'})
-});
+})
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
-});
+http.createServer(app).listen(app.get('port'), function () {
+    console.log('Express server listening on port ' + app.get('port'))
+})
